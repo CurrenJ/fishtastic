ac15154e90156910fcee728a3245b6cee520a1e3
Curren <jeandell.curren@gmail.com>
2026-08-20 23:13:06 +0100

    Allow marine compost to be placed by dispensers

--- common/src/main/java/grill24/fishtastic/FishtasticDispenseBehaviors.java
+++ common/src/main/java/grill24/fishtastic/FishtasticDispenseBehaviors.java
@@ -8,12 +8,19 @@ import net.minecraft.core.Direction;
 import net.minecraft.core.dispenser.BlockSource;
 import net.minecraft.core.dispenser.OptionalDispenseItemBehavior;
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
+import net.minecraft.world.item.context.DirectionalPlaceContext;
 import net.minecraft.world.level.block.DispenserBlock;
 import net.minecraft.world.level.block.state.BlockState;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class FishtasticDispenseBehaviors {
+    private static final Logger LOGGER = LoggerFactory.getLogger(FishtasticDispenseBehaviors.class);
+
     private FishtasticDispenseBehaviors() {}
 
     public static void registerDispenseBehaviors() {
@@ -48,5 +55,27 @@ public class FishtasticDispenseBehaviors {
         DispenserBlock.registerBehavior(Items.GOLDEN_SHOVEL, flipMarineCompost);
         DispenserBlock.registerBehavior(Items.DIAMOND_SHOVEL, flipMarineCompost);
         DispenserBlock.registerBehavior(Items.NETHERITE_SHOVEL, flipMarineCompost);
+
+        DispenserBlock.registerBehavior(FishtasticBlocks.MARINE_COMPOST.value(), new OptionalDispenseItemBehavior() {
+            @Override
+            protected ItemStack execute(BlockSource source, ItemStack dispensed) {
+                this.setSuccess(false);
+
+                Item item = dispensed.getItem();
+                if (item instanceof BlockItem blockItem) {
+                    Direction facing = source.state().getValue(DispenserBlock.FACING);
+                    BlockPos targetPos = source.pos().relative(facing);
+                    Direction clickedFace = source.level().isEmptyBlock(targetPos.below()) ? facing : Direction.UP;
+
+                    try {
+                        this.setSuccess(blockItem.place(new DirectionalPlaceContext(source.level(), targetPos, facing, dispensed, clickedFace)).consumesAction());
+                    } catch (Exception e) {
+                        LOGGER.error("Error trying to place marine compost at {}", targetPos, e);
+                    }
+                }
+
+                return dispensed;
+            }
+        });
     }
 }
